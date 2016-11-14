100b73f38e4c19e61cfc2965a7af291bce2b3dfb
b5710546232 <nattapat.su@ku.th>
2016-11-14 22:42:52 +0700

    route delete post put get

--- server.js
+++ server.js
@@ -1,20 +1,84 @@
+"use stric"
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+const User = require('./app/models/user');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080
+
+const mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDB');
 
 // ROUTES FOR API
 //
 const router = express.Router()
 
+// middleware to use for all requests
+router.use((req,res,next)=>{
+  console.log('something');
+  next() // make sure go to the next routes don't stop here.
+})
+
+
+
 router.get('/',(req,res)=>{
   res.json({msg:"Hello is me"})
 })
 
+router.route('/users')
+.post((req,res)=>{
+  let user = new User()
+  user.name = req.body.name
+  user.save((err)=>{
+    if(err){
+      res.send(err)
+    }
+    res.json({message:'User created'})
+  })
+})
+
+.get((req,res)=>{
+  User.find((err,users)=>{
+    if (err)
+    res.send(err)
+    res.json(users)
+
+  })
+})
+
+router.route('/users/:user_id')
+.get((req,res)=>{
+  User.findById(req.params.user_id,(err,user)=>{
+    if(err)
+    res.send(err)
+    res.json(user)
+
+  })
+})
+.put((req,res)=>{
+  User.findById(req.params.user_id,(err,user)=>{
+    if(err)
+    res.send(err)
+    user.name = req.body.name
+
+    user.save((err)=>{
+      if(err)
+      res.send(err)
+      res.json({message:'User updated'})
+    })
+  })
+})
+.delete((req,res)=>{
+  User.remove({
+    _id:req.params.user_id
+  },(err,user)=>{
+    if(err)
+    res.send(err)
+    res.json({message:"Deleted success"})
+  })
+})
 // Register routes
 app.use('/api',router)
 
