f1c0c2acfe4022b3133cd28d60a8f42bff09579e
b5710546232 <nattapat.su@ku.th>
2016-11-14 21:33:01 +0700

    basic set up

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 8080
+
+// ROUTES FOR API
+//
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+  res.json({msg:"Hello is me"})
+})
+
+// Register routes
+app.use('/api',router)
+
+app.listen(port,()=>{
+  console.log('Server start @ port'+port);
+})
